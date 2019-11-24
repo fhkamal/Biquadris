c4a31544374d55268c26da40c3f84fe503a97fad
fhkamal <fhkamal@student.cs.uwaterloo.ca>
2019-11-24 15:44:24 -0500

    Pushing block constuctors

--- block/jblock.h
+++ block/jblock.h
@@ -1,10 +1,11 @@
 #ifndef JBLOCK_H_
 #define JBLOCK_H_
-#include "cell.h"
 #include "block.h"
 
 class JBlock: public Block {
 	public:
-		void rotate(std::string direction, std vector<std vector<int>> coor);
-}
+		void movement(std::string dir);
+		void rotate(std::string direction);
+		JBlock(Board& Board);
+};
 #endif
