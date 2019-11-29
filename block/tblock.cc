8e45f845fb3dde56d0a696a1b290ab66c78025f4
Arif A <aaazmi@uwaterloo.ca>
2019-11-28 21:11:22 -0500

    refactored block movement

--- block/tblock.cc
+++ block/tblock.cc
@@ -46,161 +46,97 @@ void TBlock::movement(string dir)
         }
         // call temp block constructor
         TBlock tmp = TBlock();
+        
+	// Set the new Block to current block position
+	tmp.one = one;
+	tmp.two = two;
+	tmp.three = three;
+	tmp.four = four;
+	tmp.topLeft = topLeft;
+	tmp.topRight = topRight;
+	tmp.bottomLeft = bottomLeft;
+	tmp.bottomRight = bottomRight;
 
-        // Set the new Block to current block position
-        tmp.one = one;
-        tmp.two = two;
-        tmp.three = three;
-        tmp.four = four;
+	if (dir == "right")
+	{
+		// Shift the block 1 block to right
+		tmp.one = board->getGrid()[(one->getCoordinates().first)][one->getCoordinates().second + 1];
+		tmp.two = board->getGrid()[(two->getCoordinates().first)][two->getCoordinates().second + 1];
+		tmp.three = board->getGrid()[(three->getCoordinates().first)][three->getCoordinates().second + 1];
+		tmp.four = board->getGrid()[(four->getCoordinates().first)][four->getCoordinates().second + 1];
+		tmp.bottomLeft = board->getGrid()[(bottomLeft->getCoordinates().first)][bottomLeft->getCoordinates().second + 1];
+		tmp.bottomRight = board->getGrid()[(bottomRight->getCoordinates().first)][bottomRight->getCoordinates().second + 1];
+		tmp.topLeft = board->getGrid()[(topLeft->getCoordinates().first)][topLeft->getCoordinates().second + 1];
+		tmp.topRight = board->getGrid()[(topRight->getCoordinates().first)][topRight->getCoordinates().second + 1];
+	}
+	else if (dir == "left")
+	{
+		// Shift the block 1 block to left
+		tmp.one = board->getGrid()[(one->getCoordinates().first)][one->getCoordinates().second - 1];
+		tmp.two = board->getGrid()[(two->getCoordinates().first)][two->getCoordinates().second - 1];
+		tmp.three = board->getGrid()[(three->getCoordinates().first)][three->getCoordinates().second - 1];
+		tmp.four = board->getGrid()[(four->getCoordinates().first)][four->getCoordinates().second - 1];
+		tmp.bottomLeft = board->getGrid()[(bottomLeft->getCoordinates().first)][bottomLeft->getCoordinates().second - 1];
+		tmp.bottomRight = board->getGrid()[(bottomRight->getCoordinates().first)][bottomRight->getCoordinates().second - 1];
+		tmp.topLeft = board->getGrid()[(topLeft->getCoordinates().first)][topLeft->getCoordinates().second - 1];
+		tmp.topRight = board->getGrid()[(topRight->getCoordinates().first)][topRight->getCoordinates().second - 1];;
+	}
+	else if (dir == "down")
+	{
+		// Shift the block 1 block to left
+		tmp.one = board->getGrid()[(one->getCoordinates().first) + 1][one->getCoordinates().second];
+		tmp.two = board->getGrid()[(two->getCoordinates().first) + 1][two->getCoordinates().second];
+		tmp.three = board->getGrid()[(three->getCoordinates().first) + 1][three->getCoordinates().second];
+		tmp.four = board->getGrid()[(four->getCoordinates().first) + 1][four->getCoordinates().second];
+		tmp.bottomLeft = board->getGrid()[(bottomLeft->getCoordinates().first) + 1][bottomLeft->getCoordinates().second];
+		tmp.bottomRight = board->getGrid()[(bottomRight->getCoordinates().first) + 1][bottomRight->getCoordinates().second];
+		tmp.topLeft = board->getGrid()[(topLeft->getCoordinates().first) + 1][topLeft->getCoordinates().second];
+		tmp.topRight = board->getGrid()[(topRight->getCoordinates().first) + 1][topRight->getCoordinates().second];
+	}
 
-        if (dir == "right")
-        {
-                // // Check if blocks to the right are in bounds
-                // if (one->getCoordinates().second + 1 > 10 || two->getCoordinates().second + 1 > 10 ||
-                //      three->getCoordinates().second + 1 > 10 || four->getCoordinates().second + 1 > 10)
-                // {
-                //      return;
-                // }
+		// Check if the movement causes a collision
+	if (!collision(tmp))
+	{
+		return;
+	}
 
-                // // Check if blocks to the right are occupied
-                // if (board->getGrid()[(one->getCoordinates().first + 1)][one->getCoordinates().second + 1]->getIsOccupied() ||
-                //      board->getGrid()[(two->getCoordinates().first + 1)][two->getCoordinates().second + 1]->getIsOccupied() ||
-                //      board->getGrid()[(three->getCoordinates().first + 1)][three->getCoordinates().second + 1]->getIsOccupied() ||
-                //      board->getGrid()[(four->getCoordinates().first + 1)][four->getCoordinates().second + 1]->getIsOccupied())
-                //      return;
+	// Set the current cells to empty
+	one->setBlockType(' ');
+	one->setIsOccupied(false);
+	two->setBlockType(' ');
+	two->setIsOccupied(false);
+	three->setBlockType(' ');
+	three->setIsOccupied(false);
+	four->setBlockType(' ');
+	four->setIsOccupied(false);
 
-                // Shift the block 1 block to right
-                tmp.one = board->getGrid()[(one->getCoordinates().first)][one->getCoordinates().second + 1];
-                tmp.two = board->getGrid()[(two->getCoordinates().first)][two->getCoordinates().second + 1];
-                tmp.three = board->getGrid()[(three->getCoordinates().first)][three->getCoordinates().second + 1];
-                tmp.four = board->getGrid()[(four->getCoordinates().first)][four->getCoordinates().second + 1];
+	// Swap pointers to cells with the temporary block
+	swap(one, tmp.one);
+	swap(two, tmp.two);
+	swap(three, tmp.three);
+	swap(four, tmp.four);
+	swap(topLeft, tmp.topLeft);
+	swap(topRight, tmp.topRight);
+	swap(bottomLeft, tmp.bottomLeft);
+	swap(bottomRight, tmp.bottomRight);
 
-                // Set the current block cells to empty
-                if (!collision(tmp))
-                {
-                        return;
-                }
-                one->setBlockType(' ');
-                one->setIsOccupied(false);
-                two->setBlockType(' ');
-                two->setIsOccupied(false);
-                three->setBlockType(' ');
-                three->setIsOccupied(false);
-                four->setBlockType(' ');
-                four->setIsOccupied(false);
+	// Set temp values to sa random cell so they don't delete board cells
+	tmp.one = make_shared<Cell>(3, 1);
+	tmp.two = tmp.one;
+	tmp.three = tmp.one;
+	tmp.four = tmp.one;
+	tmp.topRight = tmp.one;
+	tmp.topLeft = tmp.one;
+	tmp.bottomRight = tmp.one;
+	tmp.bottomLeft = tmp.one;
 
-                // Swap pointers to cells with the temporary block
-
-                swap(one, tmp.one);
-                swap(two, tmp.two);
-                swap(three, tmp.three);
-                swap(four, tmp.four);
-
-                // Set temp values to sa random cell so they don't delete board cells
-                tmp.one = make_shared<Cell>(3, 1);
-                tmp.two = tmp.one;
-                tmp.three = tmp.one;
-                tmp.four = tmp.one;
-
-                // Set the new cell values
-                one->setBlockType('T');
-                one->setIsOccupied(true);
-                two->setBlockType('T');
-                two->setIsOccupied(true);
-                three->setBlockType('T');
-                three->setIsOccupied(true);
-                four->setBlockType('T');
-                four->setIsOccupied(true);
-        }
-        else if (dir == "left")
-        {
-                // Shift the block 1 block to left
-                tmp.one = board->getGrid()[(one->getCoordinates().first)][one->getCoordinates().second - 1];
-                tmp.two = board->getGrid()[(two->getCoordinates().first)][two->getCoordinates().second - 1];
-                tmp.three = board->getGrid()[(three->getCoordinates().first)][three->getCoordinates().second - 1];
-                tmp.four = board->getGrid()[(four->getCoordinates().first)][four->getCoordinates().second - 1];
-
-                // Set the current block cells to empty
-                if (!collision(tmp))
-                {
-                        return;
-                }
-                one->setBlockType(' ');
-                one->setIsOccupied(false);
-                two->setBlockType(' ');
-                two->setIsOccupied(false);
-                three->setBlockType(' ');
-                three->setIsOccupied(false);
-                four->setBlockType(' ');
-                four->setIsOccupied(false);
-
-                // Swap pointers to cells with the temporary block
-
-                swap(one, tmp.one);
-                swap(two, tmp.two);
-                swap(three, tmp.three);
-                swap(four, tmp.four);
-
-                // Set temp values to sa random cell so they don't delete board cells
-                tmp.one = make_shared<Cell>(3, 1);
-                tmp.two = tmp.one;
-                tmp.three = tmp.one;
-                tmp.four = tmp.one;
-
-                // Set the new cell values
-                one->setBlockType('T');
-                one->setIsOccupied(true);
-                two->setBlockType('T');
-                two->setIsOccupied(true);
-                three->setBlockType('T');
-                three->setIsOccupied(true);
-                four->setBlockType('T');
-                four->setIsOccupied(true);
-        }
-        else if (dir == "down")
-        {
-                // Shift the block 1 block to left
-                tmp.one = board->getGrid()[(one->getCoordinates().first) + 1][one->getCoordinates().second];
-                tmp.two = board->getGrid()[(two->getCoordinates().first) + 1][two->getCoordinates().second];
-                tmp.three = board->getGrid()[(three->getCoordinates().first) + 1][three->getCoordinates().second];
-                tmp.four = board->getGrid()[(four->getCoordinates().first) + 1][four->getCoordinates().second];
-
-                // Set the current block cells to empty
-                if (!collision(tmp))
-                {
-                        canDown = false;
-                        return;
-                }
-                one->setBlockType(' ');
-                one->setIsOccupied(false);
-                two->setBlockType(' ');
-                two->setIsOccupied(false);
-                three->setBlockType(' ');
-                three->setIsOccupied(false);
-                four->setBlockType(' ');
-                four->setIsOccupied(false);
-
-                // Swap pointers to cells with the temporary block
-
-                swap(one, tmp.one);
-                swap(two, tmp.two);
-                swap(three, tmp.three);
-                swap(four, tmp.four);
-
-                // Set temp values to sa random cell so they don't delete board cells
-                tmp.one = make_shared<Cell>(3, 1);
-                tmp.two = tmp.one;
-                tmp.three = tmp.one;
-                tmp.four = tmp.one;
-
-                // Set the new cell values
-                one->setBlockType('T');
-                one->setIsOccupied(true);
-                two->setBlockType('T');
-                two->setIsOccupied(true);
-                three->setBlockType('T');
-                three->setIsOccupied(true);
-                four->setBlockType('T');
-                four->setIsOccupied(true);
-        }
+	// Set the new cell values
+	one->setBlockType('T');
+	one->setIsOccupied(true);
+	two->setBlockType('T');
+	two->setIsOccupied(true);
+	three->setBlockType('T');
+	three->setIsOccupied(true);
+	four->setBlockType('T');
+	four->setIsOccupied(true);
 }
