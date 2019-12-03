89f03d48cfb8fc3f6ac16db4959c7c2e5e8cd23d
aaazmi <aaazmi@uwaterloo.ca>
2019-12-03 01:21:44 -0500

    board, block, cell documentation

--- block/block.h
+++ block/block.h
@@ -9,11 +9,8 @@
 class Block
 {
 protected:
+	// what type of block it is
 	const char let;
-	// Cell *one;
-	// Cell *two;
-	// Cell *three;
-	// Cell *four;
 
 	// cell's that a block occupies
 	std::shared_ptr<Cell> one;
@@ -29,31 +26,41 @@ protected:
 	
 	// Board that the block is on
 	std::shared_ptr<Board> board;
+
 	// Can the block move down one row
 	bool canDown;
+
 	// Length of the smallest bounding rectangle
 	const int length;
+
 	// Level the block was created
 	int levelCreated;
 
 public:
 	// Returns block's cells 
 	std::vector<std::shared_ptr<Cell>> getCells();
+
 	// Getters and Setters
 	void setCanDown(bool b);
 	bool getCanDown();
 	int getLevelCreated();
 	char getLet();
+
 	// Checks if the block will be inside of the board after movement
 	bool inBounds(std::string dir);
+
 	// Checks if b collides with any blocks on the current board
 	bool collision(Block &b);
+
 	// Moves the block in direction dir
 	virtual void movement(std::string dir);
+
 	// Constructor
 	Block(char let, std::shared_ptr<Board> board, int length);
+
 	// Destructor
 	virtual ~Block();
+
 	// Rotates a block based on direction
 	virtual void rotate(std::string direction);
 };
--- board.cc
+++ board.cc
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-// Initialize the board
-void Board::init() {  
+void Board::init() {
+    // Create cells for the grid	
     for (int i = 0; i < height; i++) {
         vector<shared_ptr<Cell>> row;
         for (int j = 0; j < width; j++) {
@@ -11,33 +11,48 @@ void Board::init() {
         }
         grid.push_back(row);
     }
-
+    // make the text display board
     td = make_shared<TextDisplay>();
 
 }
 
-// Clear the rows starting from the bottom if 
+ 
 int Board::clearRow() {
     int count = 0;
     for (int i = height - 1; i >= 0; i--) {
         bool deleteRow = true;
+	
+	// if any of the rows are not occupied it will stop checking
         for (int j = 0; j < width && deleteRow; j++) {
             deleteRow = grid[i][j]->getIsOccupied(); 
         }
+	
+	// if all the rows are occupied deleteRow will remain true and 
+	// we will clear the row
         if (deleteRow) {
+	    // temp row created to put at the top of the board
 	    vector<shared_ptr<Cell>> c;
             for (int k = 0; k < width; k++) {
+	       // set row being cleared field's to empty
                grid[i][k]->setIsOccupied(false);
+	       // initialize new empty row
 	       c.emplace_back(make_shared<Cell>(0, k));              
             }
+	    
+	    // Shifts the cells coordinates after a row has been cleared
 	    for (int j = i; j >= 0; j--){
 		for (int k = 0;  k < width; k++){
 			grid[j][k]->setCoordinates(j + 1, k);
 		}
 	    }
+
+	    // erases the row that was cleared
             grid.erase(grid.begin() + i);
+	    // adds an empty row of cells to the beginning of the grid
             grid.emplace(grid.begin(), c);
             count++;
+	    // increment row so it checks the location that was just cleared
+	    // to make sure we aren't skipping rows
             i++;
         }
 
@@ -45,25 +60,6 @@ int Board::clearRow() {
     td->updateDisplay(*this, false);
     return count;
 }
-
-bool Board::tooTall() {
-    for (int i = 0; i < width; i++) {
-        if (grid[2][i]->getIsOccupied()) return true;
-    }
-    return false;
-}
-
-// bool Board::validMove(unique_ptr<Block> b) {
-
-// }
-
-// void Board::setDisplay(unique_ptr<TextDisplay> td) {
-
-// }
-
-// void Board::setPiece(unique_ptr<Block> b) {
-
-// }
 
 shared_ptr<TextDisplay> Board::getTextDisplay(){
 	return td;
